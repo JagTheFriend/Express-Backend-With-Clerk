ada88d34011d1bfc6a8f6ee4f2e8d2d7ab5c24d0
JagTheFriend <JagTheFriend12@gmail.com>
2024-04-30 10:24:41 +0200

    Added clerk middleware
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,36 @@
-import express from "express";
+import {
+  ClerkExpressRequireAuth,
+  type RequireAuthProp,
+} from "@clerk/clerk-sdk-node";
+import cors from "cors";
+import "dotenv/config"; // To read CLERK_SECRET_KEY
+import express, {
+  type Application,
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
 
-const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const app: Application = express();
 
-app.get("/", (_req, res) => {
-  res.status(200).json({ message: "Hello world" });
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.use(ClerkExpressRequireAuth());
+
+// Use the strict middleware that raises an error when unauthenticated
+app.get("/protected-route", (req: RequireAuthProp<Request>, res: Response) => {
+  res.json(req.auth);
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(401).json({ message: "Unauthenticated!" });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
