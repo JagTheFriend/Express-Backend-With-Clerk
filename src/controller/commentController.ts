6182650907d4cd4b34a002b8ff5f3b71247fb1f2
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-04 13:32:13 +0200

    Added code for the comment controller
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/controller/commentController.ts
+++ src/controller/commentController.ts
@@ -1,17 +1,55 @@
+import type { Request, Response } from "express";
+import prisma from "../prismaClient";
+
 export class CommentController {
-  public getComments() {
-    console.log("getComments");
+  public async getComments(req: Request, res: Response) {
+    const commentData = await prisma.comment.findMany({
+      where: {
+        postId: req.body.postId,
+      },
+      take: 10,
+      orderBy: { createdAt: "desc" },
+    });
+    return res.status(200).json({ message: "get-comments", data: commentData });
   }
 
-  public createComment() {
-    console.log("createComment");
+  public async createComment(req: Request, res: Response) {
+    await prisma.comment.create({
+      data: {
+        body: req.body.body,
+        authorId: req.body.authorId,
+        postId: req.body.postId,
+      },
+    });
+    return res.status(200).json({ message: "create-comment" });
   }
 
-  public deleteComment() {
-    console.log("deleteComment");
+  public async deleteComment(req: Request, res: Response) {
+    await prisma.comment.delete({
+      where: {
+        id: req.body.id,
+        AND: {
+          postId: req.body.postId,
+          authorId: req.body.authorId,
+        },
+      },
+    });
+    return res.status(200).json({ message: "delete-comment" });
   }
 
-  public updateComment() {
-    console.log("updateComment");
+  public async updateComment(req: Request, res: Response) {
+    await prisma.comment.update({
+      where: {
+        id: req.body.id,
+        AND: {
+          postId: req.body.postId,
+          authorId: req.body.authorId,
+        },
+      },
+      data: {
+        body: req.body.body,
+      },
+    });
+    return res.status(200).json({ message: "update-comment" });
   }
 }
