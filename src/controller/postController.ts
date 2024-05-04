cd440c2f52fc8d7dd0e628f8c1ee68525b61de29
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-04 12:32:17 +0200

    Created controller
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

---
+++ src/controller/postController.ts
@@ -0,0 +1,17 @@
+export class PostController {
+  public getPosts(): void {
+    console.log("getPosts");
+  }
+
+  public createPost(): void {
+    console.log("createPost");
+  }
+
+  public deletePost(): void {
+    console.log("deletePost");
+  }
+
+  public updatePost(): void {
+    console.log("updatePost");
+  }
+}
