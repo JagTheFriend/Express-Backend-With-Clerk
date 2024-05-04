366a2a373e7b76cb16e9ec82ccaf82d2686988a1
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-04 12:32:25 +0200

    Use controller
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,17 +1,19 @@
 import { Router } from "express";
+import { PostController } from "../controller/postController";
 import type { Routes } from "../interface";
 
 export class PostRoute implements Routes {
   public router = Router();
+  public post = new PostController();
 
   constructor() {
     this.initializeRoutes();
   }
 
   private initializeRoutes() {
-    this.router.get("/get-posts");
-    this.router.post("/create-post");
-    this.router.post("/delete-post/:id");
-    this.router.post("/update-post/:id");
+    this.router.get("/get-posts", this.post.getPosts);
+    this.router.post("/create-post", this.post.createPost);
+    this.router.post("/delete-post/:id", this.post.deletePost);
+    this.router.post("/update-post/:id", this.post.updatePost);
   }
 }
