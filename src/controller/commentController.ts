5ac8fe516173a153504ab77fc55b73a8a944f139
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-04 12:34:18 +0200

    Created controller for comments
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

---
+++ src/controller/commentController.ts
@@ -0,0 +1,17 @@
+export class CommentController {
+  public getComments() {
+    console.log("getComments");
+  }
+
+  public createComment() {
+    console.log("createComment");
+  }
+
+  public deleteComment() {
+    console.log("deleteComment");
+  }
+
+  public updateComment() {
+    console.log("updateComment");
+  }
+}
--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -1,17 +1,19 @@
 import { Router } from "express";
+import { CommentController } from "../controller/commentController";
 import type { Routes } from "../interface";
 
 export class CommentRoute implements Routes {
   public router = Router();
+  public comment = new CommentController();
 
   constructor() {
     this.initializeRoutes();
   }
 
   private initializeRoutes() {
-    this.router.get("/get-comments/:id");
-    this.router.post("/create-comment");
-    this.router.post("/delete-comment/:id");
-    this.router.post("/update-comment/:id");
+    this.router.get("/get-comments/:id", this.comment.getComments);
+    this.router.post("/create-comment", this.comment.createComment);
+    this.router.post("/delete-comment/:id", this.comment.deleteComment);
+    this.router.post("/update-comment/:id", this.comment.updateComment);
   }
 }
