f0f7c36f9748f5754f5ad81fbcd69246136a496d
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-05 10:42:04 +0200

    Receive userId of current user from clerk
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/controller/commentController.ts
+++ src/controller/commentController.ts
@@ -1,3 +1,4 @@
+import type { RequireAuthProp } from "@clerk/clerk-sdk-node";
 import type { Request, Response } from "express";
 import prisma from "../prismaClient";
 
@@ -13,37 +14,37 @@ export class CommentController {
     return res.status(200).json({ message: "get-comments", data: commentData });
   }
 
-  public async createComment(req: Request, res: Response) {
+  public async createComment(req: RequireAuthProp<Request>, res: Response) {
     await prisma.comment.create({
       data: {
         body: req.body.body,
-        authorId: req.body.authorId,
+        authorId: req.auth.userId,
         postId: req.body.postId,
       },
     });
     return res.status(200).json({ message: "create-comment" });
   }
 
-  public async deleteComment(req: Request, res: Response) {
+  public async deleteComment(req: RequireAuthProp<Request>, res: Response) {
     await prisma.comment.delete({
       where: {
         id: req.body.id,
         AND: {
           postId: req.body.postId,
-          authorId: req.body.authorId,
+          authorId: req.auth.userId,
         },
       },
     });
     return res.status(200).json({ message: "delete-comment" });
   }
 
-  public async updateComment(req: Request, res: Response) {
+  public async updateComment(req: RequireAuthProp<Request>, res: Response) {
     await prisma.comment.update({
       where: {
         id: req.body.id,
         AND: {
           postId: req.body.postId,
-          authorId: req.body.authorId,
+          authorId: req.auth.userId,
         },
       },
       data: {
